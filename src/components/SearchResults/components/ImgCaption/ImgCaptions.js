461f1bd2eb8e07a125f2d5aef5aa2188f79767a4
gslav27 <v.golyaka@ukr.net>
2018-09-04 22:02:22 +0300

    delete unused function and change class component to functional

--- src/components/SearchResults/components/ImgCaption/ImgCaptions.js
+++ src/components/SearchResults/components/ImgCaption/ImgCaptions.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 
@@ -11,56 +11,48 @@ import imgCaptionsStyle from './ImgCaptions.style';
 const styles = imgCaptionsStyle();
 
 
-class ImgCaption extends Component {
-  handleOnTagClick = (tag) => {
-    this.props.fetchSearchText(tag);
-    this.props.fetchPage(1);
-    this.props.fetchImages();
-  }
+const ImgCaption = (props) => {
+  const {
+    img,
+    imgIndex,
+    classes,
+  } = props;
 
-  render() {
-    const {
-      img,
-      imgIndex,
-      classes,
-    } = this.props;
+  const imgTags = (
+    <div className={classes.gridListTileBarTitle}>
+      <ImgTags tagsData={img.tags} />
+    </div>
+  );
 
-    const imgTags = (
-      <div className={classes.gridListTileBarTitle}>
-        <ImgTags tagsData={img.tags} />
+  const imgAuthor = (
+    <div className={classes.gridListTileBarSubtitle}>
+      <span>
+        {'by: '}
+        <a
+          className={classes.barTitleTag}
+          href={`https://pixabay.com/users/${img.user}-${img.user_id}/`}
+          target='_blank'
+          title={`'${img.user}' profile at Pixabay`}
+        >
+          {img.user}
+        </a>
+      </span>
+    </div>
+  );
+
+
+  return (
+    <div className={classes.gridListTileBarRoot}>
+      <div className={classes.gridListTileBarText}>
+        {imgTags}
+        {imgAuthor}
       </div>
-    );
-
-    const imgAuthor = (
-      <div className={classes.gridListTileBarSubtitle}>
-        <span>
-          {'by: '}
-          <a
-            className={classes.barTitleTag}
-            href={`https://pixabay.com/users/${img.user}-${img.user_id}/`}
-            target='_blank'
-            title={`'${img.user}' profile at Pixabay`}
-          >
-            {img.user}
-          </a>
-        </span>
-      </div>
-    );
-
-
-    return (
-      <div className={classes.gridListTileBarRoot}>
-        <div className={classes.gridListTileBarText}>
-          {imgTags}
-          {imgAuthor}
-        </div>
-        <FavoritesButton
-          img={img}
-          imgIndex={imgIndex}
-        />
-      </div>
-    );
-  }
-}
+      <FavoritesButton
+        img={img}
+        imgIndex={imgIndex}
+      />
+    </div>
+  );
+};
 
 export default withStyles(styles)(ImgCaption);
